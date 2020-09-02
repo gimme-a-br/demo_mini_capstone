c40c1e71a1c946b0bcf3f684631f536002bcfd85
Peter Jang <peter.x.jang@gmail.com>
2020-09-01 20:36:11 -0500

    Add single product actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/all_products_path" => "products#all_products_action"
+
+    get "/first_product_path" => "products#first_product_action"
+    get "/second_product_path" => "products#second_product_action"
+    get "/third_product_path" => "products#third_product_action"
   end
 end
