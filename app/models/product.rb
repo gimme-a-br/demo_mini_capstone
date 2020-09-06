4a55aed62a1308227d0f2fbac26616e6d21d336c
Peter Jang <peter.x.jang@gmail.com>
2020-09-06 11:55:41 -0500

    Add model methods for price calculations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("%B%e, %Y")
+  end
+
+  def is_discounted?
+    price <= 10
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
