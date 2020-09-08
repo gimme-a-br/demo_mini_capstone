65672e4f7fe5e3ba28a6a1541d321d98bcd22c69
Peter Jang <peter.x.jang@gmail.com>
2020-09-07 21:10:37 -0500

    Change price data type to decimal for money

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_020303) do
+ActiveRecord::Schema.define(version: 2020_09_08_020501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 9, scale: 2
     t.string "image_url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
