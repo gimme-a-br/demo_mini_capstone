1b70d25b291a0699eb9f7821efb60881cb47fede
Peter Jang <peter.x.jang@gmail.com>
2020-09-09 20:48:12 -0500

    Add validation for valid image filetypes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, length: { in: 2..500 }
+  validates_format_of :image_url, :with => %r{\.(png|jpg|jpeg)$}i, :message => "must have a valid filetype", multiline: true
 
   def friendly_updated_at
     updated_at.strftime("%B%e, %Y")
